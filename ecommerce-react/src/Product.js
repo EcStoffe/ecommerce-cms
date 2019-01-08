18663854aa418c95618980e9bbe58da763967e50
Christoffer <jo.christoffer@gmail.com>
2019-01-08 21:05:24 +0100

    created state for updatecart(and more)

--- ecommerce-react/src/Product.js
+++ ecommerce-react/src/Product.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 import {NavLink} from "react-router-dom";
+import AddToCartButton from "./AddToCartButton";
 
 class Product extends Component {
     constructor(props) {
         super(props);
+
+    }
+
+    addToCart() {
+        let itemsInCart = JSON.parse(localStorage.getItem("itemsInCart")) || [];
+        itemsInCart.push(this.props.product);
+        localStorage.setItem("itemsInCart", JSON.stringify(itemsInCart));
+        console.log("your item: ", itemsInCart);
+        this.props.addToCart(itemsInCart)
     }
 
     render() {
         const product = this.props.product;
-        let imageURL = `http://localhost:1337${product.Image.url}`;
+        let imageURL = `http://192.168.99.100:8083/api/collections/get/Products?${product.Image}`;
+
 
         return (
-            <NavLink to={"/product/"+product.id} id="container" className="flex-container">
+
+            <div id="container" className="flex-container">
+            <NavLink to={"/product/"+product._id}>
                 <h2>Name: {product.Name} </h2>
                 <p>Description: {product.Description} </p>
                 <p>Stock: {product.Stock} </p>
                 <p>Category: {product.Category} </p>
                 <h3>Price: {product.Price} </h3>
                 <img src={imageURL} />
-                {/*<button onClick="goToItem">More info</button>
-                <button onClick="orderItem">Order</button>*/}
+                <p>Rating: {product.Rating} </p>
             </NavLink>
+                <AddToCartButton addToCart={this.addToCart.bind(this, product)}/>
+            </div>
         )
     }
 }
